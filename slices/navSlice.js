ab55f5585e1129e7cdabb46a4ec65b172c148f12
Richard Harvey <theerichardharvey@gmail.com>
2022-08-28 20:11:41 -0400

    Initial commit

---
+++ slices/navSlice.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const navSlice = () => {
+  return (
+    <View>
+      <Text>navSlice</Text>
+    </View>
+  )
+}
+
+export default navSlice

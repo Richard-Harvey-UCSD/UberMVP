e9ec34511ceacf08fe27f41b0c630fb818e057dd
Richard Harvey <theerichardharvey@gmail.com>
2022-08-28 20:26:38 -0400

    Setup Redux

--- slices/navSlice.js
+++ slices/navSlice.js
@@ -1,12 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { createSlice } from '@reduxjs/toolkit';
 
-const navSlice = () => {
-  return (
-    <View>
-      <Text>navSlice</Text>
-    </View>
-  )
-}
+const initialState = {
+  origin: null,
+  destination: null,
+  travelTimeInformation: null,
+};
 
-export default navSlice
+export const navSlice = createSlice({
+  name: 'nav',
+  initialState,
+  reducers: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+    setTravelTimeInformation: (state, action) => {
+      state.travelTimeInformation = action.payload;
+    },
+  }
+});
+
+export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.actions;
+
+export const selectOrigin = state => state.nav.origin;
+export const selectDestination = state => state.nav.destination;
+export const selectTravelTimeInformation = state => state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
